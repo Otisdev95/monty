d73e162e6db624bc6b2000f6a5f1db2dd038ed29
otis <olaliotis95@gmail.com>
2023-03-23 02:37:45 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- nop.c
+++ nop.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 
 /**
- * f_nop - function that does nothing
+ * monty_nop - function that does nothing
  * @head: double head pointer to the stack~
  * @counter: line count
  *
  * Return: nothing
  */
 
-void f_nop(stack_t **head, unsigned int counter)
+void monty_nop(stack_t **head, unsigned int counter)
 {
 	(void) counter;
 	(void) head;
